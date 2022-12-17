b2a2cccd02cf7700097a3567ecc641a562866821
Gabriel <gabrielzuli22@gmail.com>
2022-12-16 22:07:46 -0300

    Adicionano HTML e OOP no projeto

---
+++ classes/jogo.py
@@ -0,0 +1,5 @@
+class Jogo:
+    def __init__(self, nome, categoria, console) -> None:
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
--- jogoteca.py
+++ jogoteca.py
@@ -1,1 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from classes.jogo import Jogo
+
+app = Flask(__name__)
+
+@app.route('/inicio')
+def ola():
+
+    jogo1 = Jogo('Persona 5', 'JRPG', 'Playstation 4')
+    jogo2 = Jogo('God of War', 'Aventura', 'Plastation 4')
+    jogo3 = Jogo('Halo', 'FPS', 'Xbox')
+
+    lista_jogos = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
+
+app.run()
