f53fba54eab151caf6d870615e6feed4e56737ee
Gabriel <gabrielzuli22@gmail.com>
2022-12-18 23:40:19 -0300

    Adicionando usuarios ao sistema e verificação para eles

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 from classes.jogo import Jogo
+from classes.usuario import Usuario
 
 app = Flask(__name__)
 with open('security/secret_key.txt') as f:
@@ -11,6 +12,14 @@ jogo3 = Jogo('Halo', 'FPS', 'Xbox')
 
 lista_jogos = [jogo1, jogo2, jogo3]
 
+usuario1 = Usuario("Igor Batista", "Igão", "argentina_campea")
+usuario2 = Usuario("Ana Letícia", "Leh", "gosto_de_churrasco")
+usuario3 = Usuario("Ezio Auditore", "O mio todos", "florenca")
+
+usuarios = {usuario1.apelido: usuario1,
+            usuario2.apelido: usuario2,
+            usuario3.apelido: usuario3}
+
 # Rotas
 @app.route('/')
 def index():
@@ -18,8 +27,8 @@ def index():
 
 @app.route('/novo')
 def novo():
-    if 'usuario_logado' not in session: #or session['usuario_logado'] == None:
-        flash('Por favor, faça login para continuar')
+    if 'usuario_logado' not in session:
+        flash('Faça login para continuar')
         return redirect(url_for('login', proxima=url_for('novo')))
     return render_template('novo.html', titulo='Novo Jogo')
 
@@ -43,19 +52,21 @@ def login():
 
 @app.post('/autenticar')
 def autenticar():
-    if 'alohomora' == request.form['senha']:
-        session['usuario_logado'] = request.form['usuario']
-        flash(session['usuario_logado'] + ' logado com sucesso!')
-        proxima_pagina = request.form['proxima']
-        return redirect(proxima_pagina)
-    
-    flash('Usuario não cadastrado')
-    return redirect(url_for('login'))
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if usuario.senha == request.form['senha']:
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
+
+    flash('Usuário ou senha inválidos')
+    proxima = request.args.get('proxima')
+    if not proxima:
+        proxima = url_for('login')
+    return redirect(url_for('login', proxima=proxima))
 
 @app.route('/logout')
 def logout():
     session.pop('usuario_logado', None)
-    flash('Logout efetuado com sucesso!')
     return redirect(url_for('index'))
 
 # Rodar o Flask
---
+++ classes/usuario.py
@@ -0,0 +1,5 @@
+class Usuario:
+    def __init__(self, nome, apelido, senha) -> None:
+        self.nome = nome
+        self.apelido = apelido
+        self.senha = senha
