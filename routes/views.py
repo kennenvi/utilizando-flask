eca340a3b9e44ab9f35759a99ffad2d23de48351
Gabriel <gabrielzuli22@gmail.com>
2022-12-21 11:13:11 -0300

    Validando formularios com wtforms

--- routes/views.py
+++ routes/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, request, session, redirect, send_from_directory
 from app import app, db
 from models.models import Jogos, Usuarios
-from helpers.helpers import recupera_imagem, deleta_arquivo
+from helpers.helpers import recupera_imagem, deleta_arquivo, FormularioJogo
 import time
 
 
@@ -15,13 +15,19 @@ def novo():
     if 'usuario_logado' not in session:
         flash('Faça login para continuar')
         return redirect(url_for('login', proxima=url_for('novo')))
-    return render_template('novo.html', titulo='Novo Jogo')
+    form = FormularioJogo()
+    return render_template('novo.html', titulo='Novo Jogo', form=form)
 
 @app.post('/criar')
 def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
+    form = FormularioJogo(request.form)
+
+    if not form.validate_on_submit():
+        return redirect(url_for('novo'))
+
+    nome = form.nome.data
+    categoria = form.categoria.data
+    console = form.console.data
 
     jogo = Jogos.query.filter_by(nome=nome).first()
 
@@ -47,16 +53,26 @@ def editar(id):
         return redirect(url_for('login', proxima=url_for('editar')))
     
     jogo = Jogos.query.filter_by(id=id).first()
+    form = FormularioJogo()
+    form.nome.data = jogo.nome
+    form.categoria.data = jogo.categoria
+    form.console.data = jogo.console
     capa_jogo = recupera_imagem(id)
-    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo=capa_jogo)
+    return render_template('editar.html', titulo='Editando Jogo', id=id, capa_jogo=capa_jogo, form=form)
 
 @app.post('/atualizar')
 def atualizar():
+    form = FormularioJogo(request.form)
+
+    if not form.validate_on_submit():
+        flash('Não foi possível atualizar')
+        return redirect(url_for('editar', id=request.form['id']))
+
     jogo = Jogos.query.filter_by(id=request.form['id']).first()
 
-    jogo.nome = request.form['nome']
-    jogo.categoria = request.form['categoria']
-    jogo.console = request.form['console']
+    jogo.nome = form.nome.data
+    jogo.categoria = form.categoria.data
+    jogo.console = form.console.data
 
     db.session.add(jogo)
     db.session.commit()
